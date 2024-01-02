cbbdcded2565c144701bac72957f6a84dbdd2af3
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-01-02 12:42:14 +0800

    Build script.

---
+++ buildwebsite.sh
@@ -0,0 +1,14 @@
+# Set output directory to the "labwebsite" folder in the current directory
+OUTPUTDIR=labwebsite
+
+# Empty the output directory
+rm -rf $OUTPUTDIR
+
+# Run the build command
+hugo --gc -b "https://cde.nus.edu.sg/bme/biorobotics+lab" -d $OUTPUTDIR
+
+# Zip up the folder
+zip -r $OUTPUTDIR.zip $OUTPUTDIR
+
+# Copy the folder to the Downloads folder
+cp $OUTPUTDIR.zip ~/Downloads
