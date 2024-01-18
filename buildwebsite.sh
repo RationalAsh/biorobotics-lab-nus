9a007bb4c6188d97688796613611e2ccf85a9b44
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-01-18 17:52:46 +0800

    Update compile script

--- buildwebsite.sh
+++ buildwebsite.sh
@@ -4,6 +4,9 @@ OUTPUTDIR=labwebsite
 # Empty the output directory
 rm -rf $OUTPUTDIR
 
+# If a zip file exists, remove it
+rm -f $OUTPUTDIR.zip
+
 # Run the build command
 hugo --gc -b "https://cde.nus.edu.sg/bme/bioroboticslab" -d $OUTPUTDIR --minify
 
